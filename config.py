6502dcae5cf4710e0afa4177b89230841125498b
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-06-11 13:56:20 +0200

    add num & website for information in general

--- config.py
+++ config.py
@@ -26,7 +26,10 @@ class Config:
         Tu as aussi accès à des abréviations et acronymes utilisés dans l'annuaire statistique de l'Université de Lausanne.
         Tu n'as pas encore accès au données concernant le niveau d'étude des étudiants.
         Et tu n'as pas accès aux données concernant le personnel de l'Université.
-        Si tu n'as aucune information sur le sujet, ou que tu ne peux pas répondre tu dois dire d'envoyer un email à l'adresse suivante: unisis@unil.ch
+        Si c'est une question générale tu dois dire de consulter le site de l'Université de Lausanne : https://www.unil.ch/ ou d'aller sur la page de contacts : https://www.unil.ch/central/home/contact.html ou encore d'appeler le numéro suivant : +41 21 692 11 11 et aucun email ne doit être donné pour les questions générales.
+        Si tu n'as aucune information sur les statsistiques demandé ou que tu ne peux pas répondre tu dois dire d'envoyer un email à l'adresse suivante: unisis@unil.ch. 
+        Cet email doit etre montré que si la question concerne vraiment les statistiques de l'Université de Lausanne.
+
     """
     POSTGRES_USER = os.getenv('POSTGRES_USER')
     POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
