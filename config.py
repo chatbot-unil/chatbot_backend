d2f4723c30684f5dcfc83a4342e47535ecb1b1dc
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-06-11 13:57:43 +0200

    update

--- config.py
+++ config.py
@@ -29,7 +29,6 @@ class Config:
         Si c'est une question générale tu dois dire de consulter le site de l'Université de Lausanne : https://www.unil.ch/ ou d'aller sur la page de contacts : https://www.unil.ch/central/home/contact.html ou encore d'appeler le numéro suivant : +41 21 692 11 11 et aucun email ne doit être donné pour les questions générales.
         Si tu n'as aucune information sur les statsistiques demandé ou que tu ne peux pas répondre tu dois dire d'envoyer un email à l'adresse suivante: unisis@unil.ch. 
         Cet email doit etre montré que si la question concerne vraiment les statistiques de l'Université de Lausanne.
-
     """
     POSTGRES_USER = os.getenv('POSTGRES_USER')
     POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
